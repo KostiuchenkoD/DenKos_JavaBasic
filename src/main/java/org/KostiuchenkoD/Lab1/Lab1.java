ccb4b839f0a1f9be4e6da41bb8412ae8e9271fb9
Denys <fozzydealman@gmail.com>
2025-03-19 22:54:32 +0200

    Lab 2

--- src/main/java/org/KostiuchenkoD/Lab1/Lab1.java
+++ src/main/java/org/KostiuchenkoD/Lab1/Lab1.java
@@ -3,6 +3,7 @@ package org.KostiuchenkoD.Lab1;
 public class Lab1 {
 
     public static void start() {
+
         System.out.println("Hello World!");
     }
 
--- src/main/java/org/KostiuchenkoD/Main.java
+++ src/main/java/org/KostiuchenkoD/Main.java
@@ -1,12 +1,15 @@
 package org.KostiuchenkoD;
 
 import org.KostiuchenkoD.Lab1.Lab1;
+import org.KostiuchenkoD.Lab2.Lab2;
 
 public class Main {
     public static void main(String[] args) {
 
         System.out.println("Lab 1");
         Lab1.start();
+        System.out.println("Lab 2");
+        Lab2.start();
     }
 
 }
